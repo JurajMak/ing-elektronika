58fbc10cfaa2c90bb47693468f9d9ae8d383ea19
JurajMak <93321577+JurajMak@users.noreply.github.com>
2023-11-12 22:36:03 +0100

    fix: mobile contact fix

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -8,17 +8,17 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className=" grid grid-cols-12 gap-12 my-12">
-      <div className="col-start-2 col-span-10 lg:col-start-2 lg:col-span-3  ">
+    <div className=" grid grid-cols-6  xl:grid-cols-12 gap-12 my-12">
+      <div className="col-span-6 p-4 lg:col-start-2 lg:col-span-3 lg:p-0  ">
         <h3 className="text-3xl my-6 lg:mb-6">Informacije</h3>
         <ContactCard />
       </div>
-      <div className=" col-start-2 col-span-10 lg:col-start-6 lg:col-span-6 rounded-xl ">
+      <div className=" col-span-6 p-4 lg:col-start-6 lg:col-span-6 rounded-xl lg:p-0  ">
         <h3 className="text-3xl my-6 lg:mb-6">Pronađite nas na adresi</h3>
         <Map />
       </div>
 
-      <div className="col-start-2 col-span-10 lg:col-start-4 lg:col-span-5 my-24 ">
+      <div className="col-span-6 p-2  lg:col-start-4 lg:col-span-5 my-24 lg:p-0  ">
         <Card className="p-12">
           <h3 className="text-3xl font-medium">Kontakt forma </h3>
           <p className="text-md text-muted-foreground mb-4">
